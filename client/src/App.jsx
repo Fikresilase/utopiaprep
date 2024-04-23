b81873e4ae1884851501e184e03e605e521b98da
fikresilase <shitubilbila@gmail.com>
2024-04-23 22:14:52 +0300

    registration page 1st demo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,12 @@ import Resources from "./components/pricing";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
-import Leader from "./components/landingpage";
+// import Leader from "./components/landingpage";
 
 function App() {
   return <>
-  {/* <Header/><Hero/><Features/><Resources/><Footer/> */}
-  <Leader/>
+  <Header/><Hero/><Features/><Resources/><Footer/>
+  {/* <Leader/> */}
   </>;
 }
 
